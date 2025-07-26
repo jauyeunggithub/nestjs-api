eef4e5948dfa2794c8aa60258da2a0bbe9b04dd0
Ho Hang Au Yeung <mayeung@telus.net>
2025-07-26 13:22:01 -0700

    fix batch file

--- test.bat
+++ test.bat
@@ -4,11 +4,14 @@ setlocal
 echo Starting Docker containers...
 docker-compose up --build -d
 
+REM Wait a few seconds for containers to start properly
+timeout /t 5 /nobreak > nul
+
 echo Running migrations inside the app container...
-docker exec -it nestjs_app_container_name npm run migration:run
+docker exec -it app npm run migration:run
 
 echo Running tests inside the app container...
-docker exec -it nestjs_app_container_name npm run test:ci
+docker exec -it app npm run test:ci
 
 echo Stopping and removing Docker containers...
 docker-compose down
