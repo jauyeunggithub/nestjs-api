cf3ebe544cf677a967bd7c2bc7a6920b49c1b2e7
Ho Hang Au Yeung <mayeung@telus.net>
2025-07-26 13:07:21 -0700

    add missing files

--- src/polymorhpic/polymorphic.module.ts
+++ src/polymorhpic/polymorphic.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { PolymorphicService } from './polymorphic.service';
 import { PolymorphicController } from './polymorphic.controller';
+import { PolymorphicService } from './polymorphic.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
 import { Image } from './entities/image.entity';
 import { Comment } from './entities/comment.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post, Image, Comment])],
-  providers: [PolymorphicService],
   controllers: [PolymorphicController],
+  providers: [PolymorphicService],
 })
 export class PolymorphicModule {}
