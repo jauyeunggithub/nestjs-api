028660d9468a7f45be157a1258747631ce7847f5
Ho Hang Au Yeung <mayeung@telus.net>
2025-07-26 13:31:03 -0700

    fix scripts

--- test.bat
+++ test.bat
@@ -8,14 +8,13 @@ REM Wait a few seconds for containers to start properly
 timeout /t 5 /nobreak > nul
 
 echo Running migrations inside the app container...
-docker exec -it app npm run migration:run
+docker exec -it nestjs-api-app-1 npm run migration:run
 
-echo Running tests inside the app container...
-docker exec -it app npm run test:ci
+echo Running tests inside the test container...
+docker exec -it nestjs-api-test-1 npm run test:ci
 
 echo Stopping and removing Docker containers...
 docker-compose down
 
 endlocal
 echo Done.
-
